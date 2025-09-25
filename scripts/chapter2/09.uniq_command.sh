acb73bbfb224f4019dabcfebdcd258f1c3952014
arafat_shovon <arafatshovon1897@gmail.com>
2025-09-26 01:45:13 +0600

    Modified uniq command file

--- scripts/chapter2/09.uniq_command.sh
+++ scripts/chapter2/09.uniq_command.sh
@@ -19,5 +19,13 @@ sort -k 1 data.txt | uniq -c
 ../utils/print_red_line.sh "Print Only the Unique Lines"
 ../utils/print_green_line.sh "sort -k <column_number> <filename> | uniq -d"
 sort -k 1 data.txt | uniq -d
+../utils/make_block.sh
+
+
+../utils/print_red_line.sh "Some times we would want to find out unique things based on some set of characters rather than whole content"
+echo -e "u:01:GNU\Nd:04:Linux\nu:01:Bash\nu:01:Hack" > data.txt
+../utils/print_red_line.sh "Content of a file is:\n$(cat data.txt)"
+../utils/print_red_line.sh "command : sort <filename> | uniq -s <offset_character> -w <number_of_characters_to_sort_on>"
+sort data.txt | uniq -s 2 -w 2
 
 rm data.txt
